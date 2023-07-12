c8f5d363435a6091e1f7f4fcdaecdaaef2a8ab5f
ASMAAGOMA <asmaagadallaah@gmail.com>
2023-07-11 19:29:06 -0700

    0x01-python-if_else_loops_functions

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for i in range(122, 96, -1):
+    if i % 2 == 0:
+        letter = chr(i).lower()
+    else:
+        letter = chr(i).upper()
+    print("{}".format(letter), end="")
+print()
