3a0b651c4cc619da8cb958a549512466ad61ae4d
ASMAAGOMA <asmaagadallaah@gmail.com>
2023-07-11 19:52:41 -0700

    0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,8 +1,8 @@
-#!/bin/bash
-magic_calculation(a, b, c):
-if a < b:
-    return c
-elif b > c:
-    return a + b
-else:
-    return a * b - c 
+#!/usr/bin/python3
+def magic_calculation(a, b, c):
+    if a < b:
+        return c
+    elif b > c:
+        return a + b
+    else:
+        return a * b - c 
