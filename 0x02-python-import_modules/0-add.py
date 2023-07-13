54a4f3cc0944fc389210782345c9b1285771d9e6
ASMAAGOMA <asmaagadallaah@gmail.com>
2023-07-13 14:21:30 -0700

    0x02-python-import_modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import importlib
-from add_0 import add
-a = 1
-b = 2
-result = add(a, b)
-print("{} + {} ={}".format(a, b, result))
+if __name__ == "__main__":
+    import importlib
+    from add_0 import add
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{} + {} ={}".format(a, b, result))
