f53a7ed682be17045278f6e27e693ae385ae8e20
ASMAAGOMA <asmaagadallaah@gmail.com>
2023-07-15 23:00:53 -0700

    0x03-python-data_structures

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def no_c(my_string):
     char_list = list(my_string)
-    for i, char in enumerate(char_list):
+    i = 0
+    while i < len(char_list):
         if char_list[i] == 'c' or char_list[i] == 'C':
-            char_list[i] = ''
+            char_list.pop(i)
         else:
-            continue
-        my_string = ''.join(char_list)
-        return (my_string)
+            i += 1
+        return (''.join(char_list))
