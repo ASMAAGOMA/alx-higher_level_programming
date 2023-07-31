e6b4a026a0914b5f8ec437bac88bf6289559c2f7
ASMAAGOMA <asmaa gadallaah@gmail.com>
2023-07-31 20:17:05 +0300

    exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,10 +2,11 @@
 def safe_print_list(my_list=[], x=0):
     count = 0
     try:
-        if count < x:
+        while count < x:
             print(my_list[count], end = "")
             count += 1
     except IndexError:
         pass
-    print()
-    return (count)
+    finally:
+        print()
+        return (count)
