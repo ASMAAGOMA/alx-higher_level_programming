b89ea8227658515bbdd6e92766c1a62baaa4b4da
ASMAAGOMA <asmaagadallaah@gmail.com>
2023-08-01 15:04:56 -0700

    0x06-python-classes

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,25 @@
 #!/usr/bin/python3
 class Square:
     """
-    A class that  defines a square.
+    Represents a square.
+
+    This class defines a square with no attributes or methods.
+    It serves as a basic blueprint for creating square objects.
+
+    Attributes:
+    None
+
+    Methods:
+    None
+
+    Example usage:
+    # Create a square object
+    my_square = Square()
+
+    # Print the type of the square object
+    print(type(my_square))  # Output: <class '0-square.Square'>
+
+    # Print the instance variables of the square object
+    print(my_square.dict)  # Output: {}
     """
     pass
