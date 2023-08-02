26a6c503fa2d71a1939bdf45a225cb2caf1aa66b
ASMAAGOMA <asmaagadallaah@gmail.com>
2023-08-01 19:25:39 -0700

    0x06-python-classes

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Define a square"""
+
+
+class Square:
+    """Represents the sqaure"""
+
+    def __init__(self, size=0):
+        """Initialize the square.
+
+        Args:
+        size(int):The size of the square
+        """
+        self.size = size
+
+        @property
+        def size(self):
+            """Get the size"""
+            return(self.__size)
+
+        @size.setter
+        def size(self, value):
+            if not isinstance(size, int):
+                raise TypeError("size must be an integer")
+            elif size < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = size
+
+        def area(self):
+            """Return the area of the square"""
+            return (self.__size * self.__size)
