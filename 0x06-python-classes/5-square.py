d87b8381587f2fb301333202882b1d47f605ecee
ASMAAGOMA <asmaagadallaah@gmail.com>
2023-08-05 15:39:23 -0700

    0x06-python-classes

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -36,8 +36,8 @@ class Square:
 
     def my_print(self):
         """Prints in stdout the square with the character #"""
-        if self * __size == 0:
+        if self.__size == 0:
             print()
         else:
-            for i in range(self * __size):
-                print("#" * self * __size)
+            for i in range(self.__size):
+                print("#" * self.__size)
