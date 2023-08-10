e4caffe2d1e542d9b252d3f91345e185ea3fdeca
ASMAAGOMA <asmaagadallaah@gmail.com>
2023-08-10 10:14:07 -0700

    0x07-python-test_driven_development

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
+    """
+    Adds two integers
+
+    Args:
+    a (int or float): first num
+    b (int or float): second num
+
+    Returns:
+    int: the sum
+
+    Raises:
+    TypeError: a must be an integer or b must be an integer
+    """
     if type(a) not in(int, float):
         raise TypeError("a must be an integer")
     if type(b) not in(int, float):
