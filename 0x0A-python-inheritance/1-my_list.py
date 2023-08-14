faad2cda0ed6bae0df63962e953510ba82ae5b5d
ASMAAGOMA <asmaagadallaah@gmail.com>
2023-08-14 15:49:43 -0700

    0x0A-python-inheritance

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+"""Class MyList that inherits from list"""
+
+
+class MyList(list):
+
+    """Class MyList that inherits from list"""
+    def print_sorted(self):
+        sorted_l = sorted(self)
+        print(sorted_l)
