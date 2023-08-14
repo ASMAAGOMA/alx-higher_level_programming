dc03cef89d891f95fbb91fc52c4202bdca631469
ASMAAGOMA <asmaagadallaah@gmail.com>
2023-08-14 16:44:33 -0700

    0x0A-python-inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Defines a class BaseGeometry"""
+
+    def area(self):
+        """Area not implemented"""
+        raise TypeError("area() is not implemented")
