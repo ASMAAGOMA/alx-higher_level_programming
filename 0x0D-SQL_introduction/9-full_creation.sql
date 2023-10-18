a8154b7091eca96e410e34ddf96173986a865664
ASMAAGOMA <xasmaagadallaah@gmail.com>
2023-10-17 19:00:29 -0700

    DB

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- script that creates a table second_table
-CREATE TABLE second_table (id INT AUTO_INCREMENT, name VARCHAR(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table (id INT AUTO_INCREMENT, name VARCHAR(256), score INT);
 INSERT INTO second_table (name, score) VALUES ("John", 10)
 INSERT INTO second_table (name, score) VALUES ("Alex", 3)
 INSERT INTO second_table (name, score) VALUES ("Bob", 14)
