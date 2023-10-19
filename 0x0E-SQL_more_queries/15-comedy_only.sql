2ae84446569ddc92397504cc137c11effb7cdeea
ASMAAGOMA <xasmaagadallaah@gmail.com>
2023-10-18 18:26:28 -0700

    DB

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,4 +1,4 @@
-Import the database dump from hbtn_0d_tvshows
+--Import the database dump from hbtn_0d_tvshows
 SELECT title FROM tv_shows
 JOIN tv_show_genres ON id=tv_show_genres.show_id
 JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id
