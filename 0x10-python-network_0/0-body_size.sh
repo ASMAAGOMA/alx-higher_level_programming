e10dd3f0628324578a4bd90c477cb8ff4952a477
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-01-26 02:24:39 +0200

    fgg

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # displays the size of the body of the response
-curl -s -o /dev/null -w "%{size_download}" https://curl.se/docs/httpscripting.html
+curl -sI "$1" | grep -i 'content-length' | cut -d ' ' -f2
