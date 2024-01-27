ad99abd2b4fcbae4d821409863809d224ce6c6be
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-01-27 06:07:44 +0200

    :"(

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-"""
-script that takes in a letter and sends a POST request
-to http://0.0.0.0:5000/search_user
+"""A script tha:
+- takes in a letter
+- sends POST request to http://0.0.0.0:5000/search_user
 with the letter as a parameter.
 """
 import sys
 import requests
+
+
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
-        letter = ""
-    else:
-        letter = sys.argv[1]
-    value = {"q": letter}
-    r = requests.post("http://0.0.0.0:5000/search_user", data=value)
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
+
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
     try:
-        response = r.json
+        response = r.json()
         if response == {}:
             print("No result")
         else:
