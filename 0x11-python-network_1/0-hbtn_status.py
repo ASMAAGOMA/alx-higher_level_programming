ff146a140be3037b8bd712a598a19c3fa6ec8f89
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-01-27 02:40:42 +0200

    :"(

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-"""script that fetches 
-https://alx-intranet.hbtn.io/sta
 """
+script that fetches
+https://alx-intranet.hbtn.io/sta
+
+"""
+
 if __name__ == "__main__":
     import urllib.request
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    u = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(u)as response:
         body = response.read()
-        print ("Body response:")
+        print("Body response:")
         print("\t- type:", type(body))
         print("\t- content:", body)
         print("\t- utf8 content:", body.decode('utf-8'))
-    
