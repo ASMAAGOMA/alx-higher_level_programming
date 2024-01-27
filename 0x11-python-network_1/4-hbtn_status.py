6fd4f7623084761cd8b527a81f4f3c0e85851c56
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-01-27 03:39:54 +0200

    :"(

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     import requests
     u = "https://alx-intranet.hbtn.io/status"
     with requests.get(u)as response:
-        body = response.read()
+        body = response.txt
         print("Body response:")
         print("\t- type:", type(body))
         print("\t- content:", body)
