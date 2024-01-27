9ae290ec07ab26ee3d1290af79ae036c1c71f627
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-01-27 03:06:18 +0200

    :"(

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -14,4 +14,4 @@ if __name__ == "__main__":
     data = data.encode('ascii')
     req = urllib.request.Request(u, data)
     with urllib.request.urlopen(req) as response:
-        the_page = response.read().decode('utf-8')
+        print(response.read().decode('utf-8'))
