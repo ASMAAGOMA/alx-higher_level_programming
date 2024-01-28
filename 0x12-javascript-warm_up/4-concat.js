4baebed01610e5d78b21ab544a9a97a1a6f5b47b
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-01-28 20:20:53 +0200

    :"(

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 if (typeof process.argv[2] === 'undefined') {
-    console.log('undefined is undefined');
-  } else if (process.argv.length === 3) {
-    const firstArg = process.argv[2] + ' is undefined';
-    console.log(firstArg);
-  } else {
-    const twoArgs = process.argv[2] + ' is ' + process.argv[3];
-    console.log(twoArgs);
-  }
+  console.log('undefined is undefined');
+} else if (process.argv.length === 3) {
+  const firstArg = process.argv[2] + ' is undefined';
+  console.log(firstArg);
+} else {
+  const twoArgs = process.argv[2] + ' is ' + process.argv[3];
+  console.log(twoArgs);
+}
