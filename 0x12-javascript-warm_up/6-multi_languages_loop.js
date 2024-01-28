273cc6de0d85352d231d1dcfd413589de271e9d9
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-01-28 21:37:11 +0200

    :"(

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-for (const values of ['C is fun'
-, 'Python is cool',
-'JavaScript is amazing']) {
+for (const values of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
   console.log(values);
 }
