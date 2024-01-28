d7646a0e530ea36fd64e478dfdb0e057776bda22
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-01-28 23:51:59 +0200

    :"(

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,7 +3,7 @@ function fact (num) {
   if (num === 0 || isNaN(num)) {
     return 1;
   } else {
-    return num * fact (num - 1);
+    return num * fact(num - 1);
   }
 }
 console.log(fact(Number(process.argv[2])));
