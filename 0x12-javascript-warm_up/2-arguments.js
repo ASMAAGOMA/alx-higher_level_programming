182b7ae040d82d325e7071313c9c3ed6803ac94c
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-01-28 08:26:04 +0200

    :"(

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+const num = process.argv.length;
+if(num === 2){
+    console.log('No argument')
+}
+else if(num === 3){
+    console.log('Argument found')
+}
+else{
+    console.log('Arguments found')
+}
