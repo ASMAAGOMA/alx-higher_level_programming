1f9911ef12768e8392e328a8180a82da756e5ba7
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-01-29 00:42:22 +0200

    :"(

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+function sorting (args) {
+//get the numbers from line arguments and turn it into numeric values
+  const numbers = args.slice(2).map(Number);
+  const sorted = numbers.sort((a, b) => a - b);
+  if (sorted.length >= 2) {
+    return sorted[1];
+  } else {
+    return 0;
+  }
+}
+console.log(sorting(process.argv));
