9c71ab2e92d8b4af8c589124971ee8f2ce39c343
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-01-29 23:08:08 +0200

    :"(

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,8 +4,9 @@ const Hell = require('./5-square.js');
 
 class Square extends Hell {
   constructor (size) {
-  super(size, size);
+    super(size, size);
   }
+
   charPrint (c) {
     for (let i = 0; i < this.height; i++) {
       console.log(Array(this.width + 1).join(c || 'X'));
