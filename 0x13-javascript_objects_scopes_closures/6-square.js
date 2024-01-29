8acc0e78a18919f59ccd6bc8e16651403d345766
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-01-29 10:21:02 +0200

    :"(

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const Hell = require('./5-square.js');
+
+class Square extends Hell {
+  constructor (size) {
+  super(size, size);
+  }
+  charPrint (c) {
+    for (let i = 0; i < this.height; i++) {
+      console.log(Array(this.width + 1).join('c || X'));
+    }
+  }
+}
+module.exports = Square;
