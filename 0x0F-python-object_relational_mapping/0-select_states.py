b0b148e9e756e3668e56f0157f13bf9abcdb277c
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-01-30 21:43:00 +0200

    :"(

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,7 @@ import sys
 if __name__ == "__main__":
     username, password, name = sys.argv[1:]
     db = MySQLdb.connect(host="localhost", port=3306,
-                         password=password, myDb=name)
+                         password=password, db=name)
     curs = db.cursor()
     curs.execute("SELECT id, name FROM statws ORDERD BY id ASC")
     for row in curs.fetchall():
