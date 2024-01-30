dfa717c809d551daee6934b1776f342de3a901e9
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-01-30 22:18:58 +0200

    :"(

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+script that lists all states from the database hbtn_0e_0_usa
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username, password, name = sys.argv[1:]
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         password=password, db=name)
+    curs = db.cursor()
+    curs.execute("SELECT * FROM states WHERE\
+                  name LIKE '{}' ORDER BY id ASC;".format(search))
+    for state in curs.fetchall():
+        print(state)
