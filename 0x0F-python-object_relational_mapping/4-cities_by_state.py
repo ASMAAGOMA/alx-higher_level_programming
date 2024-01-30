75174b2d107d202d9623a8071366a5a24c62a60a
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-01-30 22:58:05 +0200

    :"(

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+script that lists all states from the database hbtn_0e_0_usa
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username, password, name = sys.argv[1:]
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         password=password, db=name)
+    curs = db.cursor()
+    curs.execute(("SELECT cities.id, cities.name"
+                  "FROM cities INNER JOIN states ON cities,state.id = state.id"
+                  " ORDER BY cities.id ASC;"))
+    for row in curs.fetchall():
+        print(row)
