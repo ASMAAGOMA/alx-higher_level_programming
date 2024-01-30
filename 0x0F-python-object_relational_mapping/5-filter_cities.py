aa1e6b968115fef1527a9ae666d0789e789d3eb1
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-01-30 23:31:14 +0200

    :"(

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Script that lists all cities from the database hbtn_0e_4_usa,
+along with their corresponding state names.
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username, password, name, search = sys.argv[1:]
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, passwd=password, db=name)
+    curs = db.cursor()
+    count = curs.execute(("SELECT cities.id, cities.name, states.name "
+                          "FROM cities "
+                          "INNER JOIN states ON cities.state_id = states.id "
+                          "WHERE states.name = '{search}'"
+                          "ORDER BY cities.id ASC;"))
+    for state in curs.fetchall():
+        count -= 1
+        if (count == 0):
+            print(state[0], end='')
+        else:
+            print(state[0], end=', ')
