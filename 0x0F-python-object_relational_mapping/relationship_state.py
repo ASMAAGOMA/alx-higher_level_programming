774bf49d03d89b000fd86ec5d2f2f14fee727f22
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-01-31 06:25:35 +0200

    :"(

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+State Model
+"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    The State Model.
+    """
+    tablename = "states"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", back_populates="state",
+                          cascade="all, delete, delete-orphan")
