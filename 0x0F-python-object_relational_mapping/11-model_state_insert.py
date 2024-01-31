9908d8070a6a0d09d2cdc8a926b89ca5b6204201
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-01-31 03:59:06 +0200

    :"(

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
 
 """
-Define a class that maps to the 'states' table in the database.
+Script to add a new state to the 'states' table in the database and print its ID.
 """
 
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Baze, State
+from model_state import Base, State
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    session = sessionmaker(bind=engine)
-    session = session()
 
-    new = State(name="Louisiana")
-    session.add(new)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name="Louisiana")
+
+    session.add(new_state)
     session.commit()
-    print(new.id)
+
+    print("New state ID:", new_state.id)
