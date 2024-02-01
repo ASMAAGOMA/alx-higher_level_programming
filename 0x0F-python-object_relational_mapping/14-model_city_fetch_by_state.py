8e4ac93a13da0843e432129370a939e5cad40145
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-02-01 03:02:40 +0200

    h

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -8,7 +8,8 @@ the database and print its ID.
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import City, Base, State
+from model_state import Base, State
+from model_city import City
 
 
 if __name__ == "__main__":
