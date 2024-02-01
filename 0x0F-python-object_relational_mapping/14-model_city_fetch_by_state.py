e38ba25f2dc6e3806527ab1e736789f0ea4c9295
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-02-01 03:06:42 +0200

    :"(

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -5,13 +5,16 @@ Script to add a new state to the 'states' table in
 the database and print its ID.
 """
 
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+
+from model_city import Base, State, City
 
 
 if __name__ == "__main__":
+    
+    import sys
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
