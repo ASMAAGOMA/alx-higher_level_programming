1a257bb7f87a98408e62a15e2cffc8b39c61e575
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-02-01 02:45:01 +0200

    :"(

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -8,16 +8,16 @@ the database and print its ID.
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+from model_state import Base, State, City
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    host, password, name = sys.argv[1:]
+    uri = f"mysql+mysqldb://{host}:{password}@localhost/{name}"
+    engine = create_engine(uri, pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    query = session.query(State).filter(State.name.like('%a%'))
+    query = session.query(State).join(City).order_by(City.id)
     for state in query:
-        session.delete(state)
-    session.commit()
+        for city in state.cities:
+            print(f"{state.name}: ({city.id}) {city.name}")
