d776e691280d70b3e4e00890b04268bd53ac5ed3
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-02-01 06:04:02 +0200

    :"(

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
 
 """
-Script to list all states in the database along with their cities.
+A script that lists all states in the database.
 """
 
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from model_city import Base, State, City
 
 
 if __name__ == "__main__":
-    engine = create_engine(f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}'
-                           , pool_pre_ping=True)
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import create_engine
+    import sys
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    host, password, name = sys.argv[1:]
+    uri = f"mysql+mysqldb://{host}:{password}@localhost/{name}"
+    engine = create_engine(uri, pool_pre_ping=True)
 
+    session = sessionmaker(bind=engine)()
     query = session.query(State).join(City).order_by(City.id)
 
     for state in query:
-        print(f"{state.name}:")
         for city in state.cities:
-            print(f"({city.id}) {city.name}")
+            print(f"{state.name}: ({city.id}) {city.name}")
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,30 +1,17 @@
 #!/usr/bin/python3
-
 """
-City Model: Defines the City model representing a city entity in the database.
+City Model
 """
-
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from model_state import State, Base
 
-Base = declarative_base()
-
 
 class City(Base):
     """
-    Represents a city entity in the database.
-
-    Attributes:
-        id (int): The primary key of the city.
-        name (str): The name of the city.
-        state_id (int): The foreign key referencing
-        the state the city belongs to.
-        state (State): The relationship to the State
-        model representing the state the city belongs to.
+    The City Model.
     """
-
     __tablename__ = "cities"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
