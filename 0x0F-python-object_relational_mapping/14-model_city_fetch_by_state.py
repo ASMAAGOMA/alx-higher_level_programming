6e416bda1ffdfff57fc03ec4ce1933766f66f3c0
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-02-01 02:40:52 +0200

    :"(

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,27 +1,23 @@
-"""
-Script to list all states in the database along with their cities.
-"""
-
 #!/usr/bin/python3
 
 """
-Script to list all states in the database along with their cities.
+Script to add a new state to the 'states' table in
+the database and print its ID.
 """
 
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_city import Base, State, City
-
+from model_state import Base, State
 
 if __name__ == "__main__":
-    host, password, name = sys.argv[1:]
-    uri = f"mysql+mysqldb://{host}:{password}@localhost/{name}"
-    engine = create_engine(uri, pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
 
-    session = sessionmaker(bind=engine)()
-    query = session.query(State).join(City).order_by(City.id)
-
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query = session.query(State).filter(State.name.like('%a%'))
     for state in query:
-        for city in state.cities:
-            print(f"{state.name}: ({city.id}) {city.name}")
+        session.delete(state)
+    session.commit()
