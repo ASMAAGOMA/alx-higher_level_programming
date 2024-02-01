275220e2b22ded66617531752d9ecc084a754a31
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-02-01 02:58:48 +0200

    :"(

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -8,7 +8,8 @@ the database and print its ID.
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+from model_state import Base, State, City
+
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
